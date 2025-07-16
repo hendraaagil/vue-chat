52e5966cadb70c8c866b0aebfbb889ceb67de084
hendraaagil <hendraaagil@gmail.com>
2025-07-16 21:15:13 +0700

    feat: add not found view

---
+++ src/views/not-found.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex flex-1 items-center justify-center">
+    <div class="text-center">
+      <h3 class="mb-2 text-2xl font-bold text-slate-900">Page Not Found</h3>
+      <p class="text-slate-500">The page you are looking for does not exist.</p>
+      <RouterLink to="/" class="font-medium text-blue-500 hover:underline"
+        >Go back to Home</RouterLink
+      >
+    </div>
+  </div>
+</template>
